73bf16b0fd1cbc2db6d9b869a1bfc88eae6f6720
WLDToken <151801254+WLDToken@users.noreply.github.com>
2023-11-27 15:32:48 +0800

    t

---
+++ index_files/config.js
@@ -0,0 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// This file is a way to override env variables for stage and production
+
+
+}
+/*
+     FILE ARCHIVED ON 15:03:12 May 31, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:03:51 Nov 27, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 640.049
+  exclusion.robots: 0.103
+  exclusion.robots.policy: 0.091
+  cdx.remote: 0.077
+  esindex: 0.012
+  LoadShardBlock: 577.749 (3)
+  PetaboxLoader3.datanode: 651.577 (5)
+  load_resource: 127.598
+  PetaboxLoader3.resolve: 31.232
+  loaddict: 83.281
+*/
